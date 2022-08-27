d3affca482e4d7b2652ddabe57781f8c8cfb783d
Olesia Ivanova <ivanova.ov_87@mail.ru>
2022-08-27 22:28:01 +0300

    второй коммит тринадцатого урока

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const render = function() {
     todoList.innerHTML = '';
     todoCompleted.innerHTML = '';
 
-    toDoData.forEach(function(item) {
+    toDoData.forEach(function(item, index) {
         const li = document.createElement('li');
         li.classList.add('todo-item');
         li.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
@@ -20,25 +20,43 @@ const render = function() {
 
         if (item.completed) {
             todoCompleted.append(li);
+            localStorage.setItem("toDoData", JSON.stringify(toDoData));
         } else {
             todoList.append(li);
+            localStorage.setItem("toDoData", JSON.stringify(toDoData));
         }
 
         li.querySelector('.todo-complete').addEventListener('click', function() {
             item.completed = !item.completed;
             render();
-        });        
+            localStorage.setItem("toDoData", JSON.stringify(toDoData));
+        });
+        
+        li.querySelector('.todo-remove').addEventListener('click', function() {
+            toDoData.splice(index, 1);
+            render();     
+            localStorage.setItem("toDoData", JSON.stringify(toDoData));
+        });
+        
     });
 };
 
 todoControl.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const newToDo = {
-        text: headerInput.value,
-        completed: false
-    };
-    toDoData.push(newToDo);
-    headerInput.value = '';
-    render();
+    if (headerInput.value !== '') {
+        const newToDo = {
+            text: headerInput.value,
+            completed: false
+        };
+        
+        toDoData.push(newToDo);
+        headerInput.value = '';
+        render();
+    }
 });
+copyToDoData = JSON.parse(localStorage.getItem("toDoData"));
+for (let i = 0; i < copyToDoData.length; i++) {
+    toDoData.push(copyToDoData[i]);
+}
+render();
